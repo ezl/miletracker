49c032c6c3e056a7e17adfdcb7cd50012b222453
ezl <ericzliu@gmail.com>
2012-12-23 15:14:12 -0600

    login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/$', 'accounts.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+
+def login(request, template_name="registration/login.html"):
+    ctx = dict()
+    print request.POST
+    return render_to_response(
+        template_name, RequestContext(request, ctx))
