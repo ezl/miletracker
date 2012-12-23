65f591ff9e7cdb7d05b3fbb8f37b0ecdd33630fb
ezl <ericzliu@gmail.com>
2012-12-23 09:17:42 -0600

    sexy login input straight up stolen from launchrock: thanks launchrock

--- miletracker/urls.py
+++ miletracker/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
    # (r'', include('miletracker.apps.')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('accounts.urls')),
 
     url(r'^$',
         direct_to_template, {
