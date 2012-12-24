ef392402c7c856cb85bedc2d12b0f85b21dfd092
ezl <ericzliu@gmail.com>
2012-12-24 12:02:19 -0600

    wtf -- trouble processing request.post

--- accounts/views.py
+++ accounts/views.py
@@ -7,20 +7,26 @@ from django.contrib.auth.views import login as auth_login
 # from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def login(request, template_name="registration/login.html"):
+    # import pdb; pdb.set_trace()
     if request.method == "POST":
+        return HttpResponse("hello1")
         username = request.POST.get("username", "").lower()
         password = request.POST.get("password", "")
 
+        return HttpResponse("hello3")
         try:
             user = User.objects.get(username__iexact=username)
         except User.DoesNotExist:
             user = None
 
+        return HttpResponse("hello4")
         if user:
+            return HttpResponse("hello5")
             response = auth_login(request)
             return response
         else:
             # create that sucker
+            return HttpResponse("hello6")
             print "5"*50
             #return HttpResponseRedirect(reverse("log"))
 
